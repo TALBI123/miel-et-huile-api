0d24abe159fa41e8faa17daf1b7ef4c081fb5758
TALBI123 <mohamedtalbitop@gmail.com>
2025-09-11 00:41:27 +0100

    change type parmeter shema

--- src/routes/auth/auth.route.ts
+++ src/routes/auth/auth.route.ts
@@ -5,7 +5,7 @@ import { validate } from "../../middlewares/validate";
 import { verifyToken } from "../../middlewares/auth";
 const router = Router();
 
-router.post("/login", validate(authSchema.pick({email:true,password:true})), login);
-router.post("/register", validate(authSchema), register);
+router.post("/login", validate({ schema: authSchema.pick({ email: true, password: true }) }), login);
+router.post("/register", validate({ schema: authSchema }), register);
 router.get("/logout", verifyToken, logout);
 export default router;
