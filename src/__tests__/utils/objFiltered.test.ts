4215a861a1fb1e1742b3a9f1ab4097b6602f84b2
TALBI123 <mohamedtalbitop@gmail.com>
2025-11-15 21:20:07 +0100

    add supertest & jest

--- src/__tests__/utils/objFiltered.test.ts
+++ src/__tests__/utils/objFiltered.test.ts
@@ -1,41 +1,41 @@
 import { buildRelationsFilter, objFiltered } from "../../utils/filter";
 describe("teste objFiltered returner les donne qui changes de meme champs", () => {
-  // it("retourne les champs identiques entre oldObj et newObj", () => {
-  //   const oldObj = { name: "Miel", price: 20 };
-  //   const newObj = { name: "Miel", price: 25 };
-  //   const result = objFiltered(oldObj, newObj);
-  //   expect(result).toEqual({ price: 25 });
-  // });
-  // it("retourne newObj ", () => {
-  //   const oldObj = { name: "Miel", price: 20 };
-  //   const newObj = { name: "Thym", price: 25 };
-  //   const result = objFiltered(oldObj, newObj);
+  it("retourne les champs identiques entre oldObj et newObj", () => {
+    const oldObj = { name: "Miel", price: 20 };
+    const newObj = { name: "Miel", price: 25 };
+    const result = objFiltered(oldObj, newObj);
+    expect(result).toEqual({ price: 25 });
+  });
+  it("retourne newObj ", () => {
+    const oldObj = { name: "Miel", price: 20 };
+    const newObj = { name: "Thym", price: 25 };
+    const result = objFiltered(oldObj, newObj);
 
-  //   expect(result).toEqual({ name: "Thym", price: 25 });
-  // });
-  // it("retourne vide si rien ne correspond", () => {
-  //   const oldObj = { name: "Miel", price: 20 };
-  //   const newObj = { name: "Miel" };
-  //   const result = objFiltered(oldObj, newObj);
+    expect(result).toEqual({ name: "Thym", price: 25 });
+  });
+  it("retourne vide si rien ne correspond", () => {
+    const oldObj = { name: "Miel", price: 20 };
+    const newObj = { name: "Miel 3aso" };
+    const result = objFiltered(oldObj, newObj);
 
-  //   expect(result).toEqual({});
-  // });
-  //  it("retourne vide si rien ne correspond", () => {
-  //   const oldObj = { name: "Miel" };
-  //   const newObj = { name: "Miel", price: 20 };
-  //   const result = objFiltered(oldObj, newObj);
-  //   console.log(result, "result");
-  //   expect(result).toEqual({});
-  // });
+    expect(result).toEqual({name: "Miel 3aso"});
+  });
+   it("retourne vide si rien ne correspond", () => {
+    const oldObj = { name: "Miel" };
+    const newObj = { name: "Miel", price: 20 };
+    const result = objFiltered(oldObj, newObj);
+    console.log(result, "result");
+    expect(result).toEqual({name: "Miel"});
+  });
+  
+  it("retourne vide si rien ne correspond", () => {
+    const oldObj = { name: "Miel" };
+    const newObj = { slug: "miel-bio" } as any;
+    const result = objFiltered(oldObj, newObj);
 
-  // it("retourne vide si rien ne correspond", () => {
-  //   const oldObj = { name: "Miel" };
-  //   const newObj = { slug: "miel-bio" } as any;
-  //   const result = objFiltered(oldObj, newObj);
-
-  //   expect(result).toEqual({});
-  // });
-  it.skip("retourne isActive si changer", () => {
+    expect(result).toEqual({});
+  });
+  it("retourne isActive si changer", () => {
     const oldObj = { name: "Miel", isActive: false };
     const newObj = { isActive: true } as any;
     const result = objFiltered(oldObj, newObj);
